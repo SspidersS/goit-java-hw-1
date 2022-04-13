fdccf2a4f6bd0b1f7b5fbcff43354eb601cdb475
SspidesS <99414766+SspidesS@users.noreply.github.com>
2022-04-14 00:21:52 +0200

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,7 +1,6 @@
  package main.java;
-class main {
-    public static void main(String[] args) {
+class Main {
+    public static void main(String args[]) {
         System.out.println("Hello Java");
     }
 }
-
